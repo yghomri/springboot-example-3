5c050d79efd6fb700ff04afbacf957e559458c05
Yacine <g_yacine@outlook.com>
2022-09-21 14:13:11 -0500

    Delete ConsultationRepository.java

--- demo/demo/src/main/java/com/example/demo/repositories/ConsultationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.Consultation;
-
-public interface ConsultationRepository extends JpaRepository<Consultation, Long> {
-    
-}
