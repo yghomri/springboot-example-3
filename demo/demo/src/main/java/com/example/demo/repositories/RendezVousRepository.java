1e0feacc3839b22c2f6403d9d78f51aceaf34c71
Yacine <g_yacine@outlook.com>
2022-09-21 14:13:05 -0500

    Delete RendezVousRepository.java

--- demo/demo/src/main/java/com/example/demo/repositories/RendezVousRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entities.RendezVous;
-
-public interface RendezVousRepository extends JpaRepository <RendezVous, Long> {
-    
-}
