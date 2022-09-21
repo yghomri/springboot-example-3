fbe061b9d19ad36745cb7c742b38ee3904f172b9
Yacine <g_yacine@outlook.com>
2022-09-20 21:52:36 -0500

    Add files via upload
    updated structure

---
+++ demo/src/main/java/com/example/demo/repositories/MedecinRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.entities.Medecin;
+
+public interface MedecinRepository extends JpaRepository<Medecin, Long> {
+    
+}
---
+++ demo/src/main/java/com/example/demo/repositories/PatientRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.entities.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    
+}
